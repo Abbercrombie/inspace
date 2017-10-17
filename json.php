c6b18d21828cd3dfd9eaec87052d33ffdc5bf2e7
Abbercrombie <32276948+Abbercrombie@users.noreply.github.com>
2017-10-17 21:00:29 +0300

    Add files via upload

--- json.php
+++ json.php
@@ -1,61 +1,52 @@
 <meta charset = 'utf-8'>
 <?php
-$url = 'https://api2.nrg-tk.ru/v2/cities?lang=ru';
-$file = file_get_contents($url);
-$jsonSite=json_decode($file);
-echo '<pre>';
-$list = $jsonSite->cityList;
-
-for ($i=0;$i<302;$i++)
+require_once 'functionsJsonParser.php';
+function parser ()
 {
+    $url = 'https://api2.nrg-tk.ru/v2/cities?lang=ru';
+    $file = file_get_contents($url);
 
-    $city = ($list[$i]->name);
-    echo "Город: $city <br>";
-
-    $countWarehouse = count($list[$i]->warehouses);
-    echo "Кол-во отделений : $countWarehouse <br>";
-
-        foreach ($list[$i]->warehouses as $warehouses)
-        {
-
-            $worktimeBudnie = ($warehouses->workTime[0]);
-            $worktimeBudnieFrom = ($worktimeBudnie->begin);
-            $worktimeBudnieTo = ($worktimeBudnie->end);
-
-            $worktimeSubbota = ($warehouses->workTime[5]);
-            $worktimeSubbotaFrom = ($worktimeSubbota->begin);
-            $worktimeSubbotaTo = ($worktimeSubbota->end);
-            //if (!empty($worktimeBudnieFrom and $worktimeBudnieTo and $worktimeSubbotaFrom and $worktimeSubbotaTo))
-                $grafik = "График отделения  ПН-ПТ с $worktimeBudnieFrom до $worktimeBudnieTo , Суббота с $worktimeSubbotaFrom до $worktimeSubbotaTo <br> ";
-
-            $phone = ($warehouses->phone);
-            //if (!empty($phone))
-            //echo "Телефон отделения № $r : $phone";
-        }
-                foreach ($list[$i] as $list2)
-                {
-
-
-                    for ($r=0;$r<$countWarehouse;$r++)
-                    {
-                        $titleWarehouse = ($list2[$r]->title);
-                        $adress = ($list2[$r]->address);
-                        if (!empty($titleWarehouse and $adress))
-                            echo " отделение № $r) : $titleWarehouse , Адресс : $adress , $grafik ,$phone <br>";
-
-
-                    }
+    if ($file!==false)
+    {
+        $jsonSite = json_decode($file);
+        echo '<pre>';
+        $list = $jsonSite->cityList;
+        $numOfCities = count($list);
+        for ($i = 0; $i < $numOfCities; $i++) {
+            if (!empty($list[$i]->defaultIdWare)) {
+                $city = ($list[$i]->name);
+                echo "Город : $city <br>";
+                $countWarehouse = count($list[$i]->warehouses);
+                echo "Кол-во отделений : $countWarehouse <br>";
+                foreach ($list[$i]->warehouses as $warehouses) {
+                    $worktimeBudnie = ($warehouses->workTime[0]);
+                    $worktimeBudnieFrom = ($worktimeBudnie->begin);
+                    $worktimeBudnieTo = ($worktimeBudnie->end);
+                    $worktimeSubbota = ($warehouses->workTime[5]);
+                    $worktimeSubbotaFrom = ($worktimeSubbota->begin);
+                    $worktimeSubbotaTo = ($worktimeSubbota->end);
+                    $phone = strip_tags($warehouses->phone);
+                    $titleWarehouse = strip_tags($warehouses->title);
+                    $adress = strip_tags($warehouses->address);
+                    $grafikBudnie = "<br> График отделения ПН-ПТ с $worktimeBudnieFrom до $worktimeBudnieTo <br>";
+                    $grafikSubbota = " График отделения СБ    с $worktimeSubbotaFrom до $worktimeSubbotaTo <br> ";
+                    if (!empty($titleWarehouse and $adress))
+                        echo " <br> Отделение \"$titleWarehouse\" , Адресс : $adress  <br>";
+                    if (!empty($worktimeBudnieFrom and $worktimeBudnieTo))
+                        echo $grafikBudnie;
+                    if (!empty($worktimeSubbotaFrom and $worktimeSubbotaTo))
+                        echo $grafikSubbota;
+                    if (!empty($phone))
+                        echo "Телефон отделения : $phone <br><br>";
+                    $grafik = strip_tags($grafikBudnie . $grafikSubbota);
+                    addWarhouses($city, $titleWarehouse, $adress, $grafik, $phone);
                 }
-
-
-
-
-
-
-
-
-
-    echo '<hr>';
+                echo '<hr>';
+            }
+        }
+        echo '</pre>';
+    }
+    else echo "TROUBLE";
 }
-echo '</pre>';
+parser();
 ?>
